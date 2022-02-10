0e5b9b91b5f854ff50c8c06bf28f59afc1ed76d9
송가현 <87346297+eclipsepeach@users.noreply.github.com>
2022-02-10 16:35:00 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
  <!doctype html>
  <html>
@@ -14,6 +15,7 @@ function templateHTML(title, list, body){
  <body>
    <h1><a href="/">WEB2</a></h1>
    ${list}
+   ${control}
    ${body}
  </body>
  </html>
@@ -39,7 +41,7 @@ var app = http.createServer(function(request,response){
     //parse는 추출한다는 뜻으로 parse함수를 사용하면 쿼리스트링을 객체로 변환하여 리턴.
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
+    console.log(pathname);
 
     if(pathname === '/'){
       if(queryData.id === undefined){
@@ -47,7 +49,8 @@ var app = http.createServer(function(request,response){
             var title = 'Welcome';
             var description = 'Hello stranger.';
             var list = templateList(filelist);
-            var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,`<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`);
           response.writeHead(200);
           response.end(template);
         });
@@ -58,7 +61,8 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${queryData.id}`,'utf8', function(err, description){
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
     response.writeHead(200);
     response.end(template);
 
@@ -66,7 +70,84 @@ var app = http.createServer(function(request,response){
   });
 }
 
-  }else{
+}else if(pathname === '/create'){
+  fs.readdir('./data', function(error, filelist){
+    var title = 'WEB - create';
+    var list = templateList(filelist);
+    var template = templateHTML(title, list,
+      `<form action="/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+      </form>`,''
+    );
+  response.writeHead(200);
+  response.end(template);
+});
+}else if(pathname === '/create_process'){
+  var body="";
+
+  request.on('data', function(data){
+    body = body+data;
+  });
+  request.on('end', function(){
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8',function(err){
+      response.writeHead(302, {Location:`/?id=${title}`});
+      response.end();
+    })
+  });
+}else if(pathname === '/update'){
+  fs.readdir('./data', function(error, filelist){
+    fs.readFile(`data/${queryData.id}`,'utf8', function(err, description){
+      var title = queryData.id;
+      var list = templateList(filelist);
+      var template = templateHTML(title, list,`
+        <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+        </form>
+        `,
+      `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+response.writeHead(200);
+response.end(template);
+
+});
+});
+
+}else if(pathname === '/update_process'){
+  var body="";
+
+  request.on('data', function(data){
+    body = body+data;
+  });
+  request.on('end', function(){
+    var post = qs.parse(body);
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error){
+      fs.writeFile(`data/${title}`, description, 'utf8',function(err){
+        response.writeHead(302, {Location:`/?id=${title}`});
+        response.end();
+        })
+    })
+    console.log(post);
+  });
+
+}else{
     response.writeHead(404);
     response.end('Not found');
   }
